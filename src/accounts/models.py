9058f63a0c8cecb365ce4d08ec90a0eeb9bbbd6d
Ruben Arriazu <ruben.arsi@protonmail.ch>
2022-01-11 16:25:22 +0100

    Change identation to spaces in account/models.py

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,31 +3,31 @@ from django.contrib.auth.models import User
 
 
 class Account(models.Model):
-	user = models.OneToOneField(
-		User,
-		on_delete=models.CASCADE,
-		primary_key=True,
-	)
-	bio = models.TextField(default='')
-	profile_picture = models.ImageField(upload_to='uploads/', null=True)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    bio = models.TextField(default='')
+    profile_picture = models.ImageField(upload_to='uploads/', null=True)
 
 
 class FollowerPetition(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	possible_follower = models.ForeignKey(
-		User, on_delete=models.CASCADE,
-		related_name='following_petition'
-	)
-	petition_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    possible_follower = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='following_petition'
+    )
+    petition_time = models.DateTimeField(auto_now_add=True)
 
-	class Meta:
-		unique_together = ('user', 'possible_follower')
+    class Meta:
+        unique_together = ('user', 'possible_follower')
 
 
 class Follow(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
-	timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+    timestamp = models.DateTimeField(auto_now_add=True)
 
-	class Meta:
-		unique_together = ('user', 'follower')
+    class Meta:
+        unique_together = ('user', 'follower')
