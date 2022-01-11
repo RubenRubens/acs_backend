fbc190d940c822f8d6be002ad9eb1e6b997fcd85
Ruben Arriazu <ruben.arsi@protonmail.ch>
2022-01-11 14:27:10 +0100

    Change tabs for spaces on photos/views.py

--- src/photos/views.py
+++ src/photos/views.py
@@ -13,74 +13,74 @@ from .serializers import CommentSerializer, PostSerializer
 
 
 class PostCreate(generics.CreateAPIView):
-	queryset = Post.objects.all()
-	serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-	def perform_create(self, serializer):
-		serializer.save(author=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class PostDetail(generics.RetrieveDestroyAPIView):
-	queryset = Post.objects.all()
-	serializer_class = PostSerializer
-	permission_classes = [IsOwnerOrReadOnlyIfIsFollower]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnlyIfIsFollower]
 
 
 class CommentCreate(generics.CreateAPIView):
-	queryset = Comment.objects.all()
-	serializer_class = CommentSerializer
-	permission_classes = [FollowerPermission]
-	
-	def perform_create(self, serializer):
-		serializer.save(author=self.request.user)
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [FollowerPermission]
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class CommentRetrieve(generics.RetrieveAPIView):
-	queryset = Comment.objects.all()
-	serializer_class = CommentSerializer
-	permission_classes = [IsOwnerOrReadOnlyIfIsFollower]
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrReadOnlyIfIsFollower]
 
 
 class CommentDestroy(generics.DestroyAPIView):
-	queryset = Comment.objects.all()
-	serializer_class = CommentSerializer
-	permission_classes = [IsOwner]
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwner]
 
 
 class PostList(APIView):
-	'''
-	List all posts of a given user
-	'''
-	permission_classes = [IsOwnerOrFollower_User]
+    '''
+    List all posts of a given user
+    '''
+    permission_classes = [IsOwnerOrFollower_User]
 
-	def get(self, request, user_pk):
-		posts = Post.objects.filter(author__pk=user_pk)
-		if not posts:
-			raise Http404
-		self.check_object_permissions(request, User.objects.get(pk=user_pk))
-		serializer = PostSerializer(posts, many=True)
-		return Response(serializer.data)
+    def get(self, request, user_pk):
+        posts = Post.objects.filter(author__pk=user_pk)
+        if not posts:
+            raise Http404
+        self.check_object_permissions(request, User.objects.get(pk=user_pk))
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
 
 
 class CommentList(APIView):
-	'''
-	List all comments of a given post
-	'''
-	permission_classes = [IsOwnerOrFollower_Post]
+    '''
+    List all comments of a given post
+    '''
+    permission_classes = [IsOwnerOrFollower_Post]
 
-	def get(self, request, post_pk):
-		comments = Comment.objects.filter(post__pk=post_pk)
-		if not comments:
-			raise Http404 
-		self.check_object_permissions(request, Post.objects.get(pk=post_pk))
-		serializer = CommentSerializer(comments, many=True)
-		return Response(serializer.data)
+    def get(self, request, post_pk):
+        comments = Comment.objects.filter(post__pk=post_pk)
+        if not comments:
+            raise Http404 
+        self.check_object_permissions(request, Post.objects.get(pk=post_pk))
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
 
 
 @api_view(['GET'])
 @permission_classes([FollowerPermission])
 def feed(request):
-	following = Follow.objects.filter(follower=request.user)
-	user_feed = Post.objects.annotate(author=Subquery(following))
-	serializer = PostSerializer(user_feed, many=True)
-	return Response(serializer.data)
+    following = Follow.objects.filter(follower=request.user)
+    user_feed = Post.objects.annotate(author=Subquery(following))
+    serializer = PostSerializer(user_feed, many=True)
+    return Response(serializer.data)
