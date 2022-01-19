358ca7ddc0f8d33a21c2f7ec56a4223216708152
Ruben Arriazu <ruben.arsi@protonmail.ch>
2022-01-19 22:42:36 +0100

    Remove the admin url

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
     path('photos/', include('photos.urls'))
 ]
