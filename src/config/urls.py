929cde454f953705f5a9afdecad180c0e6051378
Ruben Arriazu <ruben.arsi@protonmail.ch>
2022-06-05 17:02:52 +0200

    Add landing page with whitenoise

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
 
 from .views import docs_view
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="config/landing_page.html")),
     path('account/', include('accounts.urls')),
     path('photos/', include('photos.urls')),
     path('docs/', docs_view.with_ui('redoc', cache_timeout=0))
